c17e91f1711cda99f7f2e5a80102f64d1de82b30
Nguyen Minh Nhat Duong <duongcoilc2004@gmail.com>
2023-04-03 00:13:15 +0700

    update

--- api/server.js
+++ api/server.js
@@ -1,22 +1,12 @@
 const jsonServer = require("json-server");
-const fs = require("fs");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3000;
 
 server.use(middlewares);
-server.use(
-  jsonServer.rewriter({
-    "/api/*": "/$1"
-  })
-);
 server.use(router);
-const dbPath = "db.json";
-if (!fs.existsSync(dbPath)) {
-  fs.writeFileSync(dbPath, "{}", "utf8");
-}
-fs.chmodSync(dbPath, "777");
-server.listen(3001, () => {
-  console.log("JSON Server is running");
+
+server.listen(port, () => {
+  console.log("App listening on post: " + port);
 });
-module.exports = server;
