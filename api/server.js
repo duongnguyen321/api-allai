a5556305a7be16c6dc44c37da4a24a92ea3fcdb0
Nguyen Minh Nhat Duong <duongcoilc2004@gmail.com>
2023-04-03 10:38:22 +0700

    Optimal handling of API Server ...

--- api/server.js
+++ api/server.js
@@ -2,11 +2,22 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 server.use(middlewares);
-server.use(router);
+server.use(jsonServer.bodyParser);
 
+server.use((req, res, next) => {
+  if (req.method === "POST" && req.path === "/chat-history") {
+    const chatHistory = router.db.get("chat-history").value();
+    if (chatHistory.length > 7) {
+      router.db.get("chat-history").remove({ id: chatHistory[0].id }).write();
+    }
+  }
+  next();
+});
+
+server.use(router);
 server.listen(port, () => {
   console.log("App listening on post: " + port);
 });
